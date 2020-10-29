f5b0f08dae4d7e766fa891f43954e865e9e337b5
Gustavo Villarroel <grvillarroel@gmail.com>
2020-10-28 22:01:15 -0400

    branch: siblinkapi v1.0 (acceso a log) essaye - 2

--- DockerRunSlog.sh
+++ DockerRunSlog.sh
@@ -1,1 +1,1 @@
-docker run --name slogx -d --rm  -m 1G -p 5000:5000 -v /opt/Siblink/Linksrv.log:/Linksrv.log:ro -e TZ="America/Argentina/Buenos_Aires" itservicegvillarroel/slogx:v1.0
+docker run --name slogx -d --rm  -m 1G -p 80:3000 -v /opt/Siblink/Linksrv.log:/Linksrv.log:ro -e TZ="America/Argentina/Buenos_Aires" itservicegvillarroel/slogx:v1.0
--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM node:latest
 WORKDIR /
 COPY . /
-CMD ["node", "app.js"] 
+CMD ["node", "src/index.js"] 
 
