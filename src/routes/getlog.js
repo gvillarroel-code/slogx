e725167ec095697fab173c1eeb8b84be28c29c9f
Gustavo Villarroel <grvillarroel@gmail.com>
2020-10-29 13:18:41 -0400

    commit 3

--- src/routes/getlog.js
+++ src/routes/getlog.js
@@ -1,5 +1,5 @@
-const fs = require('fs')
-const dateFormat = require('dateformat')
+const fs = require('fs');
+const dateFormat = require('dateformat');
 
 
 const getlog = (req, res) => {
@@ -15,27 +15,23 @@ const getlog = (req, res) => {
       result = '<h2> Log Siblink ..:  <br> '
       res.end(result + 'Error al abrir el archivo Linksrv.log </h2>')
       return 0
-    } 
+    }
     fecha = dateFormat(stats.mtime, "dd-mm-yyyy hh:MM:ss")
   })
 
+  const data = fs.readFileSync('Linksrv.log', 'UTF-8');
+  result = '<h3>Log Sinlink...:  ' + fecha + '</h3>'
+  result = result + '<a href="download">Download Linksrv.log</a>'
+  result = result + '<br>__________________________________________________________________________________<br>'
 
-  var file = fs.readFile('Linksrv.log', function (err, data) {
-    if (err) {
-      result = '<h2> Log Siblink ..:  <br> '
-      res.end(result + 'Error al abrir el archivo Linksrv.log </h2>')
-      return 0
-    } else {
+  res.write(result)
 
+  const line = data.split(/\r?\n/);
+  line.forEach((line) => {
+    res.write(line + '<br>');
+  });
 
-      result = '<h3>Log Sinlink...:  ' + fecha + '</h3>'
-      result = result + '<a href="download">Download Linksrv.log</a>'
-      result = result + '<br>__________________________________________________________________________________<br>'
-
-      result = result + data.toString().replace(/(?:\r\n|\r|\n)/g, '<br>')
-      res.end(result)
-    }
-  })
+  res.end('<br> FIN....');
 
 }
 
